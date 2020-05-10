134a4d9fe1802759d96ef07b086cdf4fcd6f822a
Michael Belete <it.michael.belete@gmail.com>
2020-05-10 08:41:36 +0300

    added graphcms api link

--- apollo/client-configs/default.js
+++ apollo/client-configs/default.js
@@ -1,7 +1,7 @@
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
-const GRAPHCMS_API = ''
+const GRAPHCMS_API = 'https://api-eu-central-1.graphcms.com/v2/ck9lclml228hb01z2170j36v3/master'
 
 export default () => ({
   link: new HttpLink({ uri: GRAPHCMS_API }),
