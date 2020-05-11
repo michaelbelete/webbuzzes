dd80b08925d80c46b169b0c72bdbd702adbc98fb
Michael Belete <it.michael.belete@gmail.com>
2020-05-11 08:00:46 +0300

    defined all queries for post

--- apollo/graphql/queries.js
+++ apollo/graphql/queries.js
@@ -1,8 +1,8 @@
 import gql from "graphql-tag"
 
-export const FETCH_POSTS = gql`
-  {
-    posts {
+export const fetchAllPosts = gql`
+  query posts($first: Int!, $skip: Int!) {
+    posts(first: $first, skip: $skip) {
       id
       title
       body
@@ -16,18 +16,46 @@ export const FETCH_POSTS = gql`
   }
 `
 
-export const FETCH_ONE_POST = gql`
-  query posts($first: Int!, $skip: Int!) {
-    posts(orderBy: dateAndTime_DESC, first: $first, skip: $skip) {
-      id
-      title
-      body
-      createdAt
-      updatedAt
-      blogger {
-        username
-        interest
+export const postCount = gql`
+  {
+    postsConnection {
+      aggregate {
+        count
       }
     }
   }
 `
+
+export const fetchOnePost = gql`
+  query post($id: String!) {
+    post(where: { id: $id }) {
+      id
+      title
+      body
+      blogger {
+        id
+        username
+      }
+    }
+  }
+`
+
+//create
+export const addPost = gql`
+  mutation($title: String!, $body: String!) {
+    createPost(data: { title: $title, body: $body }) {
+      id
+      title
+      body
+    }
+  }
+`
+//update
+export const deletePost = gql`
+  mutation($id: String!) {
+    deletePost(where: { id: $id }) {
+      id
+    }
+  }
+`
+//delete
